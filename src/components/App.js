025cbcbadb4bd9c3c41310e6c2e9f77f3072c4c5
rohan-04 <mathurrohan04@gmail.com>
2021-02-09 21:56:06 +0530

    default video set

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null }
@@ -14,7 +15,10 @@ class App extends React.Component {
             }
         });
 
-        this.setState({ videos: response.data.items })
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        })
     }
 
     onVideoSelect = (video) => {
@@ -25,9 +29,20 @@ class App extends React.Component {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                {this.state.videos.length} results found.
                 <br />
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList
+                                onVideoSelect={this.onVideoSelect}
+                                videos={this.state.videos}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
